c0377ed985155485d52adfaef93e9970c47e4033
Marcoantonio Cabrejos <marcoantonio.cabrejos@kushkipagos.com>
2022-06-13 22:27:36 -0500

    Update config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {SecurityWrapper} from "./Components/SecurityWrapper/SecurityWrapper";
+import {SecurityWrapper} from "./components/SecurityWrapper/SecurityWrapper";
 
 
 const dataRoles = [
